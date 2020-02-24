1abbf4d2081aa8c36ace56d73f809306765a17f3
alphamikle <alphamikle@gmail.com>
2020-02-24 09:51:40 +0300

    example symlink and code docs

--- assets_annotations/CHANGELOG.md
+++ assets_annotations/CHANGELOG.md
@@ -1,1 +1,2 @@
+## 1.0.1 - Example symlink and document apis
 ## 1.0.0 - Initial release
--- assets_annotations/lib/src/asset_helper.dart
+++ assets_annotations/lib/src/asset_helper.dart
@@ -1,3 +1,4 @@
+/// AstHelp is an annotation helper, which should be used for annotate your asset helper class
 class AstHelp {
   const AstHelp();
 }
